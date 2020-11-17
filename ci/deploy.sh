2d17f926d8e33676f008146fc94e9fd19012fb5f
Kumar Saurabh <itsksaurabh@gmail.com>
2020-11-17 13:14:55 +0530

    [SHIPA-620] set variables using custom flags

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -11,21 +11,61 @@ set -o pipefail
 KETCH_TAG=""
 POOL=""
 INGRESS_TYPE=""
-INGRESS_IP=""
+INGRESS_ENDPOINT=""
 APP_NAME=""
-DOCKER_REGISTRY=""
+DOCKER_IMAGE=""
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+# set variables with custom user inputs
+case $key in
+    -t|--ketch_tag)
+    KETCH_TAG="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--pool)
+    POOL="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -ig|--ingress)
+    INGRESS_TYPE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -e|--endpoint)
+    INGRESS_ENDPOINT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -a|--app)
+    APP_NAME="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -i|--image)
+    DOCKER_IMAGE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    echo "unknown option $1"
+    exit 1
+    ;;
+esac
+done
 
 # Install latest ketch binary at /usr/local/bin default location
 curl -s https://raw.githubusercontent.com/shipa-corp/ketch/main/install.sh | TAG="${KETCH_TAG}" bash
 
-# Install Cert Manager
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.3/cert-manager.yaml
-
 # Install Ketch controller
 kubectl apply -f https://github.com/shipa-corp/ketch/releases/download/v0.1.0/ketch-controller.yaml
 
-# Add a pool with ingress Traefik (default), replace ingress IP address by your ingress IP address
-ketch pool add "${POOL}"  --ingress-service-endpoint "${INGRESS_IP}" --ingress-type "${INGRESS_TYPE}"
+# Add a pool with ingress Traefik (default), replace ingress endpoint address by your ingress IP address
+ketch pool add "${POOL}"  --ingress-service-endpoint "${INGRESS_ENDPOINT}" --ingress-type "${INGRESS_TYPE}"
 
 # Create app
 ketch app create "${APP_NAME}" --pool "${POOL}"   
