a0598a7141e017e272b1d4a82fe403e16e5cc0a3
John Murphy <jmurphy@shipa.io>
2020-12-18 12:56:12 -0600

    moved mock fixtures

--- cmd/ketch/platform_delete_test.go
+++ cmd/ketch/platform_delete_test.go
@@ -1,55 +1,42 @@
 package main
 
 import (
-	"context"
-	"github.com/shipa-corp/ketch/internal/api/v1beta1"
-	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/shipa-corp/ketch/internal/api/v1beta1"
 )
 
-type mockPlatformDeleter struct {
-	getCalled    bool
-	getFn        func(name types.NamespacedName, obj runtime.Object)
-	deleteCalled bool
-	deleteFn     func(obj runtime.Object)
-}
-
-func (m *mockPlatformDeleter) Get(ctx context.Context, nn types.NamespacedName, object runtime.Object) error {
-	m.getCalled = true
-	m.getFn(nn, object)
-	return nil
-}
-
-func (m *mockPlatformDeleter) Delete(ctx context.Context, object runtime.Object, option ...client.DeleteOption) error {
-	m.deleteCalled = true
-	m.deleteFn(object)
-	return nil
+type resourceGetterDeleterMock struct {
+	resourceGetterMock
+	resourceDeleterMock
 }
 
 func TestPlatformDelete(t *testing.T) {
 	expected := "java"
-	var mp mockPlatformDeleter
-	mp.getFn = func(nn types.NamespacedName, obj runtime.Object) {
+	var mp resourceGetterDeleterMock
+	mp.resourceGetterMock.f = func(nn types.NamespacedName, obj runtime.Object) error {
 		require.Equal(t, expected, nn.Name)
 		plat, ok := obj.(*v1beta1.Platform)
 		require.True(t, ok)
 		plat.ObjectMeta.Name = nn.Name
+		return nil
 	}
-	mp.deleteFn = func(obj runtime.Object) {
+	mp.resourceDeleterMock.f = func(obj runtime.Object) error {
 		plat, ok := obj.(*v1beta1.Platform)
 		require.True(t, ok)
 		require.Equal(t, expected, plat.ObjectMeta.Name)
+		return nil
 	}
 	cmd := newPlatformDeleteCmd(&mp, ioutil.Discard)
 	cmd.SetArgs([]string{expected})
 	cmd.SetOut(ioutil.Discard)
 	err := cmd.Execute()
 	require.Nil(t, err)
-	require.True(t, mp.getCalled)
-	require.True(t, mp.deleteCalled)
+	require.True(t, mp.resourceGetterMock.called)
+	require.True(t, mp.resourceDeleterMock.called)
 }
