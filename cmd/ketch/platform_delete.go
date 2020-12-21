86122731d7b0fd454cf8d9966bc879adb19f22cd
John Murphy <jmurphy@shipa.io>
2020-12-21 10:16:02 -0600

    added sort

--- cmd/ketch/platform_delete.go
+++ cmd/ketch/platform_delete.go
@@ -8,19 +8,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func newPlatformDeleteCmd(cli resourceGetDeleter, logWriter io.Writer) *cobra.Command {
+func newPlatformDeleteCmd(cli resourceGetDeleter, out io.Writer) *cobra.Command {
 	return &cobra.Command{
 		Use:   "delete PLATFORM",
 		Short: "Delete a platform",
 		Long:  "Delete a platform that was previously added",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return deletePlatformByName(cmd.Context(), cli, args[0], logWriter)
+			return deletePlatformByName(cmd.Context(), cli, args[0], out)
 		},
 	}
 }
 
-func deletePlatformByName(ctx context.Context, cli resourceGetDeleter, platformName string, w io.Writer) error {
+func deletePlatformByName(ctx context.Context, cli resourceGetDeleter, platformName string, out io.Writer) error {
 	p, err := platformGet(ctx, cli, platformName)
 	if err != nil {
 		return fmt.Errorf("unable to find platform %q: %w", platformName, err)
@@ -28,6 +28,6 @@ func deletePlatformByName(ctx context.Context, cli resourceGetDeleter, platformN
 	if err := platformDelete(ctx, cli, p); err != nil {
 		return fmt.Errorf("unable to remove platform %q: %w", platformName, err)
 	}
-	fmt.Fprintf(w, "Successfully removed platform %q\n", platformName)
+	fmt.Fprintf(out, "Successfully removed platform %q\n", platformName)
 	return nil
 }
--- cmd/ketch/platform_list.go
+++ cmd/ketch/platform_list.go
@@ -10,13 +10,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func newPlatformListCmd(lister resourceLister, logWriter io.Writer) *cobra.Command {
+func newPlatformListCmd(lister resourceLister, out io.Writer) *cobra.Command {
 	return &cobra.Command{
 		Use:   "list",
 		Short: "List platforms",
 		Long:  "Displays list of added platforms",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return writePlatformList(cmd.Context(), lister, logWriter)
+			return writePlatformList(cmd.Context(), lister, out)
 		},
 	}
 }
@@ -28,14 +28,23 @@ func writePlatformList(ctx context.Context, lister resourceLister, out io.Writer
 	}
 	tw := tabwriter.NewWriter(out, 0, 4, 4, ' ', 0)
 	fmt.Fprintln(tw, "NAME\tIMAGE\tDESCRIPTION")
+	var lines [][]string
 	for _, platform := range platforms.Items {
 		line := []string{
 			platform.Name,
 			platform.Spec.Image,
 			platform.Spec.Description,
 		}
+		lines = append(lines, line)
+	}
+
+	if err = SortLines(lines); err != nil {
+		return err
+	}
+	for _, line := range lines {
 		fmt.Fprintln(tw, strings.Join(line, "\t"))
 	}
+
 	tw.Flush()
 	return nil
 }
