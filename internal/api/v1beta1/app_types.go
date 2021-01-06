08cdc32cc2f8e0fde6b8aa15711331a2e9424c4c
Kumar Saurabh <itsksaurabh@gmail.com>
2021-01-06 17:27:54 +0530

    [SHIPA-723] add field for active canary

--- internal/api/v1beta1/app_types.go
+++ internal/api/v1beta1/app_types.go
@@ -165,7 +165,8 @@ type AppStatus struct {
 	Pool *v1.ObjectReference `json:"pool,omitempty"`
 
 	// CurrentCanaryStep is the count for current step for a canary deployment
-	CurrentCanaryStep int `json:"current_canary_step,omitempty"`
+	CurrentCanaryStep int  `json:"current_canary_step,omitempty"`
+	IsActiveCanary    bool `json:"is_active_canary"`
 }
 
 // CanarySpec represents configuration for a canary deployment
