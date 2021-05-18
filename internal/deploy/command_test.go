9b148aa808e9493cedec68df4eb3110c2412630a
DavisFrench <davisdavis27@gmail.com>
2021-05-18 12:48:04 -0400

    WIP

--- internal/deploy/command_test.go
+++ internal/deploy/command_test.go
@@ -5,8 +5,13 @@ import (
 	"context"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	ketchv1 "github.com/shipa-corp/ketch/internal/api/v1beta1"
 	"github.com/shipa-corp/ketch/internal/build"
 	"github.com/shipa-corp/ketch/internal/docker"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"os"
+	"path"
 	"testing"
 
 	registryv1 "github.com/google/go-containerregistry/pkg/v1"
@@ -18,28 +23,35 @@ import (
 )
 
 type mockClient struct {
-	getFn    func(key client.ObjectKey) error
-	createFn func(obj runtime.Object) error
-	updateFn func(obj runtime.Object) error
+	getFn    func(counter int, obj runtime.Object) error
+	createFn func(counter int, obj runtime.Object) error
+	updateFn func(counter int, obj runtime.Object) error
+
+	getCounter int
+	createCounter int
+	updateCounter int
 }
 
-func (m mockClient) Get(ctx context.Context, key client.ObjectKey, obj runtime.Object) error {
+func (m *mockClient) Get(ctx context.Context, key client.ObjectKey, obj runtime.Object) error {
 	if m.getFn != nil {
-		return m.getFn(key)
+		m.getCounter++
+		return m.getFn(m.getCounter, obj)
 	}
 	return nil
 }
 
-func (m mockClient) Create(ctx context.Context, obj runtime.Object, opts ...client.CreateOption) error {
+func (m *mockClient) Create(ctx context.Context, obj runtime.Object, opts ...client.CreateOption) error {
 	if m.createFn != nil {
-		return m.createFn(obj)
+		m.createCounter++
+		return m.createFn(m.createCounter, obj)
 	}
 	return nil
 }
 
-func (m mockClient) Update(ctx context.Context, obj runtime.Object, opts ...client.UpdateOption) error {
+func (m *mockClient) Update(ctx context.Context, obj runtime.Object, opts ...client.UpdateOption) error {
 	if m.updateFn != nil {
-		return m.updateFn(obj)
+		m.updateCounter++
+		return m.updateFn(m.updateCounter, obj)
 	}
 	return nil
 }
@@ -83,8 +95,32 @@ func TestNewCommand(t *testing.T) {
 				"--pool", "mypool",
 				"--image", "shipa/go-sample:latest",
 			},
+			setup: func(t *testing.T) {
+				dir := t.TempDir()
+				require.Nil(t, os.Mkdir(path.Join(dir, "src"), 0700))
+				require.Nil(t, os.Chdir(dir))
+			},
 			params: &Params{
-				Client:      &mockClient{},
+				Client:      &mockClient{
+					getFn: func(counter int, obj runtime.Object) error {
+						switch counter {
+						case 1:
+							return errors.NewNotFound(v1.Resource(""), "")
+						case 2, 4:
+							_, ok := obj.(*ketchv1.Pool)
+							require.True(t, ok)
+							return nil
+						case 3:
+							_, ok := obj.(*ketchv1.Platform)
+							require.True(t, ok)
+							return nil
+
+
+						}
+
+						panic("should not reach")
+					},
+				},
 				KubeClient:  fake.NewSimpleClientset(),
 				Builder:     build.GetSourceHandler(&dockerMocker{}),
 				RemoteImage: remoteImgFn,
