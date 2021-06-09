9e56d69c93280009f4c6b9883bbddc7656dc7383
stinkyfingers <john_shenk@hotmail.com>
2021-06-09 15:01:11 -0500

    set vars

--- cli_tests/app.sh
+++ cli_tests/app.sh
@@ -3,10 +3,9 @@
 setup() {
   KETCH=$(pwd)/bin/ketch
   INGRESS=$(kubectl get svc traefik -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-  echo $INGRESS
+  FRAMEWORK="myframework"
+  APP_IMAGE="docker.io/shipasoftware/bulletinboard:1.0"
 }
-framework="myframework"
-appImage="docker.io/shipasoftware/bulletinboard:1.0"
 
 @test "help" {
   result="$($KETCH help)"
@@ -16,13 +15,13 @@ appImage="docker.io/shipasoftware/bulletinboard:1.0"
 }
 
 @test "framework create" {
-  result="$($KETCH framework add $framework --ingress-service-endpoint $INGRESS --ingress-type traefik)"
+  echo $INGRESS
+  result="$($KETCH framework add $FRAMEWORK --ingress-service-endpoint $INGRESS --ingress-type traefik)"
   [[ $result =~ "Successfully added!" ]]
 }
 
 @test "framework list" {
   result="$($KETCH framework list)"
-  echo $result
   headerRegex="NAME[ \t]+STATUS[ \t]+NAMESPACE[ \t]+INGRESS TYPE[ \t]+INGRESS CLASS NAME[ \t]+CLUSTER ISSUER[ \t]+APPS"
   dataRegex="myframework[ \t]+ketch-myframework[ \t]+traefik[ \t]+traefik"
   [[ "$result" =~ $headerRegex ]]
@@ -30,20 +29,19 @@ appImage="docker.io/shipasoftware/bulletinboard:1.0"
 }
 
 @test "app deploy" {
-  run $KETCH app deploy bulletinboard --framework $framework -i $appImage
+  run $KETCH app deploy bulletinboard --framework $FRAMEWORK -i $APP_IMAGE
   [ $status -eq 0 ]
 }
 
 @test "app list" {
   result="$($KETCH app list)"
   headerRegex="NAME[ \t]+FRAMEWORK[ \t]+STATE[ \t]+ADDRESSES[ \t]+BUILDER[ \t]+DESCRIPTION"
+  ip=$(kubectl get svc traefik -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
   dataRegex="bulletinboard[ \t]+myframework[ \t]+(created|running)[ \t]+http://bulletinboard.$INGRESS.shipa.cloud"
   echo $result
   [[ "$result" =~ $headerRegex ]]
   [[ "$result" =~ $dataRegex ]]
 }
-
-# TODO curl test
 
 @test "app remove" {
   result="$($KETCH app remove bulletinboard)"
@@ -51,6 +49,6 @@ appImage="docker.io/shipasoftware/bulletinboard:1.0"
 }
 
 @test "framework remove" {
-  result="$(echo ketch-$framework | $KETCH framework remove $framework)"
+  result="$(echo ketch-$FRAMEWORK | $KETCH framework remove $FRAMEWORK)"
   [[ $result =~ "Framework successfully removed!" ]]
 }
