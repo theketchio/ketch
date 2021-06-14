edab3e20ab4a43d297d2f36c5da695f24cc3f279
DavisFrench <davisdavis27@gmail.com>
2021-06-14 13:15:18 -0400

    Add invalidUsage statusType to better differentiate cli errors

--- internal/deploy/validations_test.go
+++ internal/deploy/validations_test.go
@@ -9,7 +9,9 @@ import (
 
 func TestErrors(t *testing.T) {
 	require.True(t, isMissing(newMissingError("oops")))
-	require.False(t, isValid(newInvalidError("oops")))
+	require.False(t, isValid(newInvalidValueError("oops")))
+	require.False(t, isValid(newInvalidUsageError("oops")))
 	require.True(t, isMissing(fmt.Errorf("some error %w", newMissingError("oops"))))
-	require.False(t, isValid(fmt.Errorf("some error %w", newInvalidError("oops"))))
+	require.False(t, isValid(fmt.Errorf("some error %w", newInvalidValueError("oops"))))
+	require.False(t, isValid(fmt.Errorf("some error %w", newInvalidUsageError("oops"))))
 }
