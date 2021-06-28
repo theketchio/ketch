212396c537ed067f96caf91b50d7690f3309a973
DavisFrench <davisdavis27@gmail.com>
2021-06-28 16:31:34 -0400

    Adjust logic in deploy

--- internal/deploy/parameters.go
+++ internal/deploy/parameters.go
@@ -204,7 +204,7 @@ func (c *ChangeSet) getProcfileName() (string, error) {
 		if err == nil && !stat.IsDir() {
 			return givenProcfile, nil
 		} else {
-			return "", newInvalidError(FlagProcFile)
+			return "", newInvalidValueError(FlagProcFile)
 		}
 	}
 	return "", newMissingError("no procfile found")
