d3bee9868889bf959267418e47d2b0ec5ffe1403
Stevan Koncar <stev.koncar@gmail.com>
2021-10-04 18:46:31 +0200

    Fix networking v1 ingress

--- cli_tests/install_ketch.sh
+++ cli_tests/install_ketch.sh
@@ -27,11 +27,7 @@ export PATH=$PATH:$(pwd)/bin
 ketch -v
 
 # helm
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-sudo apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && chmod 700 get_helm.sh && ./get_helm.sh
 
 # cert-manager
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
