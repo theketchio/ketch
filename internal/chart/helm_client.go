19f82c2d519f40d0628399a819c78f326643f236
Stevan Koncar <stev.koncar@gmail.com>
2022-01-11 11:35:13 +0100

    Fix helm update client forever blocked (#216)
    * Fix helm update client forever blocked
    
    * Fix logger
    
    * Set status to deployed instead

--- internal/chart/helm_client.go
+++ internal/chart/helm_client.go
@@ -2,16 +2,25 @@ package chart
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/go-logr/logr"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/release"
 	"helm.sh/helm/v3/pkg/storage/driver"
+	helmTime "helm.sh/helm/v3/pkg/time"
+
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	defaultDeploymentTimeout = 10 * time.Minute
 )
 
 // HelmClient performs helm install and uninstall operations for provided application helm charts.
@@ -19,6 +28,7 @@ type HelmClient struct {
 	cfg       *action.Configuration
 	namespace string
 	c         client.Client
+	log       logr.Logger
 }
 
 // TemplateValuer is an interface that permits types that implement it (e.g. Application, Job)
@@ -30,12 +40,12 @@ type TemplateValuer interface {
 }
 
 // NewHelmClient returns a HelmClient instance.
-func NewHelmClient(namespace string, c client.Client) (*HelmClient, error) {
+func NewHelmClient(namespace string, c client.Client, log logr.Logger) (*HelmClient, error) {
 	cfg, err := getActionConfig(namespace)
 	if err != nil {
 		return nil, err
 	}
-	return &HelmClient{cfg: cfg, namespace: namespace, c: c}, nil
+	return &HelmClient{cfg: cfg, namespace: namespace, c: c, log: log.WithValues("helm-client", namespace)}, nil
 }
 
 func getActionConfig(namespace string) (*action.Configuration, error) {
@@ -95,6 +105,28 @@ func (c HelmClient) UpdateChart(tv TemplateValuer, config ChartConfig, opts ...I
 	updateClient := action.NewUpgrade(c.cfg)
 	updateClient.Namespace = c.namespace
 
+	// we check releases and if lastRelease release was a while back, we set its status to Failure
+	// next helm update won't be blocked in that case
+	// this is an edge case we should cover when ketch controller pod gets restarted in middle of deploying and app
+	// in that case if helm secret (release) remains it would block next deployment forever
+	lastRelease, err := c.cfg.Releases.Last(appName)
+	if err != nil && err != driver.ErrReleaseNotFound {
+		return nil, err
+	}
+	if lastRelease != nil {
+		if lastRelease.Info.Status.IsPending() {
+			c.log.Info(fmt.Sprintf("Found pending helm release: %d", lastRelease.Version))
+			timeoutLimit := time.Now().Add(-defaultDeploymentTimeout)
+			if lastRelease.Info.FirstDeployed.Before(helmTime.Time{Time: timeoutLimit}) {
+				newStatus := release.StatusDeployed
+				c.log.Info(fmt.Sprintf("Setting status of release that has timeouted to: %s", newStatus))
+				lastRelease.SetStatus(newStatus, "manually canceled")
+				if err := c.cfg.Releases.Update(lastRelease); err != nil {
+					return nil, err
+				}
+			}
+		}
+	}
 	// MaxHistory specifies the maximum number of historical releases that will be retained, including the most recent release.
 	// Values of 0 or less are ignored (meaning no limits are imposed).
 	// Let's set it to minimal to disable "helm rollback".
