d6718b1dbb08292b0ba3d7bc3719f6415084975f
aleksej.paschenko <aleksej.paschenko@gmail.com>
2022-01-13 15:51:26 +0300

    Use a trick

--- internal/chart/helm_client_factory.go
+++ internal/chart/helm_client_factory.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/go-logr/logr"
 	"helm.sh/helm/v3/pkg/action"
@@ -17,15 +18,19 @@ import (
 // decreasing the cost of creating a new helm client.
 type HelmClientFactory struct {
 	sync.Mutex
-	configurations map[string]*action.Configuration // map[namespaceName]*action.Configuration
+	configurations              map[string]*action.Configuration // map[namespaceName]*action.Configuration
+	configurationsLastUsedTimes map[string]time.Time
+
+	lastCleanupTime time.Time
 
 	getActionConfig func(namespace string) (*action.Configuration, error)
 }
 
 func NewHelmClientFactory() *HelmClientFactory {
 	return &HelmClientFactory{
-		configurations:  map[string]*action.Configuration{},
-		getActionConfig: getActionConfig,
+		configurations:              map[string]*action.Configuration{},
+		configurationsLastUsedTimes: map[string]time.Time{},
+		getActionConfig:             getActionConfig,
 	}
 }
 
@@ -33,6 +38,8 @@ func NewHelmClientFactory() *HelmClientFactory {
 func (f *HelmClientFactory) NewHelmClient(namespace string, c client.Client, log logr.Logger) (*HelmClient, error) {
 	f.Lock()
 	defer f.Unlock()
+
+	f.cleanup()
 
 	cfg, ok := f.configurations[namespace]
 	if !ok {
@@ -43,7 +50,21 @@ func (f *HelmClientFactory) NewHelmClient(namespace string, c client.Client, log
 		}
 		f.configurations[namespace] = cfg
 	}
+	f.configurationsLastUsedTimes[namespace] = time.Now()
 	return &HelmClient{cfg: cfg, namespace: namespace, c: c, log: log.WithValues("helm-client", namespace)}, nil
+}
+
+func (f *HelmClientFactory) cleanup() {
+	if time.Since(f.lastCleanupTime) < 10*time.Minute {
+		return
+	}
+	for ns, timestamp := range f.configurationsLastUsedTimes {
+		if timestamp.Before(f.lastCleanupTime) {
+			delete(f.configurations, ns)
+			delete(f.configurationsLastUsedTimes, ns)
+		}
+	}
+	f.lastCleanupTime = time.Now()
 }
 
 func getActionConfig(namespace string) (*action.Configuration, error) {
--- internal/chart/helm_client_factory_test.go
+++ internal/chart/helm_client_factory_test.go
@@ -2,6 +2,7 @@ package chart
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"helm.sh/helm/v3/pkg/action"
@@ -13,24 +14,67 @@ func TestHelmClientFactory_NewHelmClient_VerifyCacheIsUsed(t *testing.T) {
 	getActionConfigIsCalled := map[string]int{}
 
 	factory := &HelmClientFactory{
-		configurations: map[string]*action.Configuration{},
+		configurations:              map[string]*action.Configuration{},
+		configurationsLastUsedTimes: map[string]time.Time{},
 		getActionConfig: func(namespace string) (*action.Configuration, error) {
 			getActionConfigIsCalled[namespace] += 1
 			return &action.Configuration{}, nil
 		},
 	}
+	now := time.Now()
 	cli, err := factory.NewHelmClient("my-namespace", nil, log.NullLogger{})
 	require.Nil(t, err)
 	require.NotNil(t, cli)
+	require.True(t, factory.configurationsLastUsedTimes["my-namespace"].After(now))
+	require.True(t, factory.configurationsLastUsedTimes["my-namespace"].Before(time.Now()))
 	require.Equal(t, map[string]int{"my-namespace": 1}, getActionConfigIsCalled)
 
+	now = time.Now()
 	cli, err = factory.NewHelmClient("my-namespace", nil, log.NullLogger{})
 	require.Nil(t, err)
 	require.NotNil(t, cli)
 	require.Equal(t, map[string]int{"my-namespace": 1}, getActionConfigIsCalled)
+	require.True(t, factory.configurationsLastUsedTimes["my-namespace"].After(now))
+	require.True(t, factory.configurationsLastUsedTimes["my-namespace"].Before(time.Now()))
 
 	cli, err = factory.NewHelmClient("another-namespace", nil, log.NullLogger{})
 	require.Nil(t, err)
 	require.NotNil(t, cli)
 	require.Equal(t, map[string]int{"my-namespace": 1, "another-namespace": 1}, getActionConfigIsCalled)
+}
+
+func TestHelmClientFactory_cleanup(t *testing.T) {
+	now := time.Now()
+	factory := &HelmClientFactory{
+		configurations: map[string]*action.Configuration{
+			"namespace-1": {},
+			"namespace-2": {},
+			"namespace-3": {},
+			"namespace-4": {},
+		},
+		configurationsLastUsedTimes: map[string]time.Time{
+			"namespace-1": now.Add(-20 * time.Minute),
+			"namespace-2": now.Add(-5 * time.Minute),
+			"namespace-3": now.Add(-20 * time.Minute),
+			"namespace-4": now.Add(-5 * time.Minute),
+		},
+	}
+
+	factory.lastCleanupTime = now.Add(-1 * time.Minute)
+	factory.cleanup()
+	require.Equal(t, 4, len(factory.configurations))
+	require.Equal(t, 4, len(factory.configurationsLastUsedTimes))
+	require.Equal(t, factory.lastCleanupTime, now.Add(-1*time.Minute))
+
+	factory.lastCleanupTime = now.Add(-16 * time.Minute)
+	factory.cleanup()
+	require.Equal(t, map[string]*action.Configuration{
+		"namespace-2": {},
+		"namespace-4": {},
+	}, factory.configurations)
+	require.Equal(t, map[string]time.Time{
+		"namespace-2": now.Add(-5 * time.Minute),
+		"namespace-4": now.Add(-5 * time.Minute),
+	}, factory.configurationsLastUsedTimes)
+	require.True(t, factory.lastCleanupTime.After(now))
 }
