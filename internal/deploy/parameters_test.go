b4b5e6046e0639b52562f2906ed5b303d4fff320
Kavin Aravind <kvnaravind@gmail.com>
2022-06-22 08:26:15 -0400

    [Shipa-2928] Update ketch error output (#261)
    Resolves the following bugs:
    
    1. ketch volume name is too verbose
    2. ketch prints app deploy help screen with error message repeated at the bottom. Its common for many other commands
    3. Fixed failed test-case: TestPostRenderRun/postrender_found,_patch_annotations

--- internal/deploy/parameters_test.go
+++ internal/deploy/parameters_test.go
@@ -78,7 +78,7 @@ func TestChangeSet_getVolumeName(t *testing.T) {
 		{
 			name:    "invalid volume name",
 			set:     ChangeSet{volume: stringRef("aaa/bbb")},
-			wantErr: `"volume" invalid value volume, a lowercase RFC 1123 label must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character (e.g. 'my-name',  or '123-abc', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?')`,
+			wantErr: `"volume" invalid value volume: aaa/bbb. A volume name must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character (e.g. 'my-name',  or '123-abc')`,
 		},
 	}
 
